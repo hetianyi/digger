d5b70573e7d3615d4b4b7dc484a8db908c0caa59
hetianyi <hehety@outlook.com>
2022-09-10 23:29:33 +0800

    docker buildx test

--- build-ui.sh
+++ build-ui.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-cd ui
+
 echo "build ui..."
 docker run --rm \
   -v $PWD:/app \
   -w /app \
   node:9.7.0 \
-  sh -c 'npm install --registry=https://registry.npm.taobao.org && npm run build'
+  sh -c 'cd ui && npm i && npm run build'
 
 build_status=$?
 if [ $build_status != 0 ]; then
